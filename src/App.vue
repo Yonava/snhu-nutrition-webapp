f9be98de7bf3ec612c8a303b6319ed987fb7eb48
Yona Voss-Andreae <yonavossandreae@gmail.com>
2021-10-29 02:35:26 -0400

    'v0.4'

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,47 @@
 <template>
   <div id="app" class="box">
 
-    <div v-if="toggle === false">
+
+    <div v-if="startmenu">
+      <button @click.left="startmenu = false" class="menuitems">Submit</button>
+      <button @click.left="startmenu = false" style="background-color: rgb(255, 100, 100)" class="menuitems">Skip</button>
+
+      <input type="number" />
+    </div>
+
+    <div v-if="toggle === false && startmenu === false">
       <button class="searchbar searchtab" @click="toggle = !toggle">Add More Items</button>
       <h1 style="margin-bottom: 1.5%;" class="caloriecounter">Calories:</h1>
-      <h1 style="margin-top: 1.5%;" :style="{color: rgbs}" class="caloriecounter">
-        {{calories}}
-      </h1>
+      
+      <div class="decision">
+        <h1 :style="{color: rgbCals}" class="caloriecounter">
+          {{calories}}
+        </h1>
+      </div>
 
       <p style="margin-bottom: 1%; margin-top: 1%;" class="caloriecounter">Fat:</p>
-      <p style="margin-bottom: 1%; margin-top: 1%;" :style="{color: rgbs}" class="caloriecounter">{{ calories }}</p>
+
+      <div class="decision smalldecision">
+        <p style="margin-bottom: 1%; margin-top: 1%;" :style="{color: rgbFat}" class="caloriecounter">{{ fat }}</p>
+      </div>
+
       <p style="margin-bottom: 1%; margin-top: 1%;" class="caloriecounter">Sugar:</p>
-      <p style="margin-bottom: 1%; margin-top: 1%;" :style="{color: rgbs}" class="caloriecounter">{{ calories }}</p>
+
+      <div class="decision smalldecision">
+        <p style="margin-bottom: 1%; margin-top: 1%;" :style="{color: rgbSugar}" class="caloriecounter">{{ sugar }}</p>
+      </div>
+
       <p style="margin-bottom: 1%; margin-top: 1%;" class="caloriecounter">Sodium:</p>
-      <p style="margin-bottom: 1%; margin-top: 1%;" :style="{color: rgbs}" class="caloriecounter">{{ calories }}</p>
+
+      <div class="decision smalldecision">
+        <p style="margin-bottom: 1%; margin-top: 1%;" :style="{color: rgbSodium}" class="caloriecounter">{{ sodium }}</p>
+      </div>
+
       <p style="margin-bottom: 1%; margin-top: 1%;" class="caloriecounter">Carbs:</p>
-      <p style="margin-bottom: 1%; margin-top: 1%;" :style="{color: rgbs}" class="caloriecounter">{{ calories }}</p>
+
+      <div class="decision smalldecision">
+        <p style="margin-bottom: 1%; margin-top: 1%;" :style="{color: rgbCarbs}" class="caloriecounter">{{ carbs }}</p>
+      </div>
 
       <h1 class="caloriecounter">Remove Items:</h1>
 
@@ -30,13 +56,14 @@
 
     </div>
 
-    <div v-if="toggle">
+    <div v-if="toggle && startmenu === false">
 
       <header class="box xborder">
 
           <button class="searchbar" @click="toggle = !toggle">Total</button>
 
-          <input type="text"
+          <input type="search"
+          id="search"
           class="searchbar" 
           placeholder="Search Menu Items" 
           v-model="raw_query" />
@@ -48,11 +75,11 @@
           
           <div class="decision">
 
-          <h1 :style="{color: rgbs}" class="caloriecounter">
+          <h1 :style="{color: rgbCals}" class="caloriecounter">
             {{calories}}
           </h1>
 
-        </div>
+          </div>
 
       </header>
 
@@ -84,36 +111,40 @@ export default {
   },
   data: function() {
     return {
+      userSex: '',
+      userAge: '',
+      userWeight: '',
+      startmenu: true,
       total: [],
       raw_query: '',
       calories: 0,
+      carbs: 0,
+      sodium: 0,
+      fat: 0,
+      sugar: 0,
       rgb: [0, 255, 0],
-      rgbs: 'rgb(0, 0, 0)',
+      rgbCals: 'rgb(0, 0, 0)',
+      rgbCarbs: 'rgb(0, 0, 0)',
+      rgbSodium: 'rgb(0, 0, 0)',
+      rgbSugar: 'rgb(0, 0, 0)',
+      rgbFat: 'rgb(0, 0, 0)',
       toggle: true, // for toggling between tabs in later versions
       // categories: meal periods(ie lunch)/station+cuisine type/dietary(gluten free, vegetarian, vegan)
+      // info array: cals, sodium, carbs, sugar, fat
       menu: [
-        {item: 'cesaersalad', cals: '350', category: 'lunch/saladbar/gf/vegan',
-        sodium: '30', carbs: '10', name: 'Cesaer Salad'},
-        {item: 'cheesecake', cals: '500', category: 'dessert/justdesserts/vegetarian', name: 'Cheese Cake'},
-        {item: 'veggieburrito', cals: '400', category: 'lunch/dinner/mexican/vegetarian/vegan', name: 'Veggie Burrito'},
-        {item: 'cheesepizza', cals: '300', category: 'lunch/dinner/italian/vegetarian', name: 'Cheese Pizza'},
-        {item: 'bagelwithcreamcheese', cals: '250', category: 'breakfast/jewish/vegetarian', name: 'Bagel w/ CC.'},
-        {item: 'chocolatecake', cals: '600', category: 'dessert/justdesserts/vegetarian', name: 'Chocolate Cake'},
-        {item: 'homestylechickenbowl', cals: '690', category: 'dinner/millcitygrill/gf', name: 'Chicken Bowl'},
-        {item: 'pastapennewithsauce', cals: '700', category: 'dinner/italian', name: 'Penne w/ Sauce'},
-        {item: 'test', cals: '20', category: 'breakfast/dessert/vegan/italian', name: 'Test Case'},
+        {item: 'cesaersalad', cals: '350', info: [350, 30, 50, 10, 50], category: 'lunch/saladbar/gf/vegan', name: 'Cesaer Salad'},
+        {item: 'cheesecake', cals: '500', info: [500, 50, 100, 300, 100], category: 'dessert/justdesserts/vegetarian', name: 'Cheese Cake'},
+        {item: 'veggieburrito', cals: '400', info: [400, 20, 70, 0, 40], category: 'lunch/dinner/mexican/vegetarian/vegan', name: 'Veggie Burrito'},
+        {item: 'cheesepizza', cals: '300', info: [300, 300, 500, 10, 200], category: 'lunch/dinner/italian/vegetarian', name: 'Cheese Pizza'},
+        {item: 'bagelwithcreamcheese', cals: '250', info: [250, 30, 50, 10, 200], category: 'breakfast/jewish/vegetarian', name: 'Bagel w/ CC.'},
+        {item: 'chocolatecake', cals: '600', info: [600, 30, 50, 250, 100], category: 'dessert/justdesserts/vegetarian', name: 'Chocolate Cake'},
+        {item: 'homestylechickenbowl', cals: '690', info: [690, 30, 50, 20, 100], category: 'dinner/millcitygrill/gf', name: 'Chicken Bowl'},
+        {item: 'pastapennewithsauce', cals: '700', info: [700, 30, 50, 20, 100], category: 'dinner/italian', name: 'Penne w/ Sauce'},
+        {item: 'test', cals: '20', info: [20, 30, 50, 10, 10], category: 'breakfast/dessert/vegan/italian', name: 'Test Case'},
         ],
       temp_menu: [
         // place a copy of the menu in here
-        {item: 'cesaersalad', cals: '350', category: 'lunch/saladbar/gf', name: 'Cesaer Salad'},
-        {item: 'cheesecake', cals: '500', category: 'dessert/justdesserts/vegetarian', name: 'Cheese Cake'},
-        {item: 'veggieburrito', cals: '400', category: 'lunch/dinner/mexican/vegetarian/vegan', name: 'Veggie Burrito'},
-        {item: 'cheesepizza', cals: '300', category: 'lunch/dinner/italian/vegetarian', name: 'Cheese Pizza'},
-        {item: 'bagelwithcreamcheese', cals: '250', category: 'breakfast/jewish/vegetarian', name: 'Bagel w/ CC.'},
-        {item: 'chocolatecake', cals: '600', category: 'dessert/justdesserts/vegetarian', name: 'Chocolate Cake'},
-        {item: 'homestylechickenbowl', cals: '690', category: 'dinner/millcitygrill/gf', name: 'Chicken Bowl'},
-        {item: 'pastapennewithsauce', cals: '700', category: 'dinner/italian', name: 'Penne w/ Sauce'},
-        {item: 'test', cals: '20', category: 'breakfast/dessert/vegan/italian', name: 'Test Case'},
+        
         ],
     }
   },
@@ -127,44 +158,97 @@ export default {
       return new Promise(resolve => setTimeout(resolve, ms));
     },
     // add n calories to total
-    async increase(n) { 
-     
+    async increase(key, n) { 
+    
       for (let i = 0; i < n; i++) {
+
         await this.sleep(.1)
-        this.calories++
-        this.colorCalc(this.calories)
+
+        switch (key) {
+          case 'calories':
+            this.calories++
+            this.colorCalc(key, this.calories)
+            break
+          case 'carbs':
+            this.carbs++
+            this.colorCalc(key, this.carbs)
+            break
+          case 'sodium':
+            this.sodium++
+            this.colorCalc(key, this.sodium)
+            break
+          case 'sugar':
+            this.sugar++
+            this.colorCalc(key, this.sugar)
+            break
+          case 'fat':
+            this.fat++
+            this.colorCalc(key, this.fat)
+            break
+        }
+        
       }
     },
     // remove n calories from total
-    async decrease(n) { 
-      if ((this.calories - n) >= 0) {
-        for (let i = 0; i < n; i++) {
-        await this.sleep(.1)
-        this.calories--
-        this.colorCalc(this.calories)
+    async decrease(key, n) { 
+
+      for (let i = 0; i < n; i++) {
+
+          await this.sleep(.1)
+
+          switch (key) {
+            case 'calories':
+              this.calories--
+              this.colorCalc(key, this.calories)
+              break
+            case 'carbs':
+              this.carbs--
+              this.colorCalc(key, this.carbs)
+              break
+            case 'sodium':
+              this.sodium--
+              this.colorCalc(key, this.sodium)
+              break
+            case 'sugar':
+              this.sugar--
+              this.colorCalc(key, this.sugar)
+              break
+            case 'fat':
+              this.fat--
+              this.colorCalc(key, this.fat)
+              break
+          }
+          
         }
-      }
-      else {
-        this.rgbs = `rgb(150, 0, 0)`;
-      }
+      
     },
     // CE (clear) button
     reset() { 
       location.reload();
       // this.calories = 0,
       // this.rgb = [0, 255, 0]
-      // this.rgbs = 'rgb(0, 0, 0)'
+      // this.rgbCals = 'rgb(0, 0, 0)'
       // this.raw_query = ''
     },
     print(x) {
       console.log(x)
     },
     appendTotal(obj) {
-      this.increase(obj.cals)
+
+      let keyArray = ['calories', 'sodium', 'carbs', 'sugar', 'fat']
+      for (let i = 0; i < keyArray.length; i++) {
+        this.increase(keyArray[i], obj.info[i])
+      }
+
       this.total.push(obj)
     },
     removeTotal(obj) {
-      this.decrease(obj.cals)
+     
+      let keyArray = ['calories', 'sodium', 'carbs', 'sugar', 'fat']
+      for (let i = 0; i < keyArray.length; i++) {
+        this.decrease(keyArray[i], obj.info[i])
+      }
+
       this.total.splice(this.findIndex(this.total, obj), 1)
     },
     findIndex(arr, item) {
@@ -188,15 +272,34 @@ export default {
       return count;
     },
     // adjusts color on calorie number display
-    colorCalc(calories) { 
-      let percentagegreen = ((1200-calories)/700) * 255;
-      let percentagered = (calories/500) * 255;
+    colorCalc(key, n) { 
+      this.rgb = [0, 255, 0]
+      let percentagegreen = ((1200-n)/700) * 255;
+      let percentagered = (n/500) * 255;
       this.rgb[0] = percentagered;
 
-      if (calories > 500) {
+      if (n > 500) {
         this.rgb[1] = percentagegreen;
       }
-      this.rgbs = `rgb(${this.rgb[0]}, ${this.rgb[1]}, 0)`;
+
+      switch (key) {
+        case 'calories':
+          this.rgbCals = `rgb(${this.rgb[0]}, ${this.rgb[1]}, 0)`
+          break
+        case 'sodium':
+          this.rgbSodium = `rgb(${this.rgb[0]}, ${this.rgb[1]}, 0)`
+          break
+        case 'carbs':
+          this.rgbCarbs = `rgb(${this.rgb[0]}, ${this.rgb[1]}, 0)`
+          break
+        case 'sugar':
+          this.rgbSugar = `rgb(${this.rgb[0]}, ${this.rgb[1]}, 0)`
+          break
+        case 'fat':
+          this.rgbFat = `rgb(${this.rgb[0]}, ${this.rgb[1]}, 0)`
+          break
+
+      }
     },
     // sanitizes user inputted search query
     query(raw) { 
@@ -320,12 +423,16 @@ export default {
 }
 .decision {
   font-family: consolas;
-  border: 3px solid #ffffff;
-  border-color: black;
+  border: 3px solid #000000;
   margin-top: none;
   margin: 5%;
   border-radius: 6px;
-  background-color: rgb(204, 204, 204);
+  background-color: rgb(102, 102, 102);
+}
+.smalldecision {
+  width: 100px;
+  margin-top: auto;
+  margin-bottom: auto;
 }
 .menuitems {
   margin-top: 3%;
@@ -416,6 +523,14 @@ header {
 }
 template {
   margin: none;
+}
+#search::-webkit-search-cancel-button{
+  position: relative;
+  -webkit-appearance: none;
+  height: 10px;
+  width: 10px;
+  border-radius: 50%;
+  background: rgb(251, 102, 102)
 }
 
 </style>
