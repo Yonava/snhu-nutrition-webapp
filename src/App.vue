2cd7a992a51acfc53f2ac118c36e2b4b189da4d8
Yona Voss-Andreae <yonavossandreae@gmail.com>
2021-10-30 02:54:08 -0400

    'v0.5'

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,41 @@
   <div id="app" class="box">
 
 
-    <div v-if="startmenu">
-      <button @click.left="startmenu = false" class="menuitems">Submit</button>
-      <button @click.left="startmenu = false" style="background-color: rgb(255, 100, 100)" class="menuitems">Skip</button>
+    <div v-if="startmenu" class="wborder">
 
-      <input type="number" />
+
+        <h1 class="caloriecounter">Personalize:</h1>
+
+        <div :class="'r '+crate">
+
+          <p class="caloriecounter smalldecision" style="margin-top: 4%;">Sex:</p>
+
+          <button class="crate reduced" style="background-color: rgb(73, 197, 255);" v-if="userSex === 'male'" @click.left="userSex = 'male'" :value="'male'">Male</button>
+          <button class="crate reduced" style="background-color: rgb(255,255,255);" v-else @click.left="userSex = 'male'" :value="'male'">Male</button>
+          <button class="crate reduced" style="background-color: rgb(253, 63, 158);" v-if="userSex === 'female'" @click.left="userSex = 'female'" :value="'female'">Female</button>
+          <button class="crate reduced" style="background-color: rgb(255,255,255);" v-else @click.left="userSex = 'female'" :value="'female'">Female</button>
+
+          <p class="caloriecounter smalldecision">Age:</p>
+          <input type="number" class="searchbar smalldecision" placeholder="Enter Age" v-model="userAge" />
+          <p class="caloriecounter smalldecision">Weight:</p>
+          <input type="number" style="margin-bottom: 4%;" class="searchbar smalldecision" placeholder="Enter Weight" v-model="userWeight" />
+        
+        </div>
+
+      <button @click.left="startmenu = false" :class="'total w '+crate">Submit</button>
+      <button @click.left="startmenu = false" class="total crate">Skip</button>
+
     </div>
 
     <div v-if="toggle === false && startmenu === false">
-      <button class="searchbar searchtab" @click="toggle = !toggle">Add More Items</button>
+
+    <header class="box xborder">
+
+      <button class="searchbar searchtab" style="margin-top: 3%;" @click="toggle = !toggle">Add More Items</button>
+      <button class="searchbar searchtab" style="margin-top: 3%;" @click="startmenu = true">Personalize...</button>
+
+    </header>
+
       <h1 style="margin-bottom: 1.5%;" class="caloriecounter">Calories:</h1>
       
       <div class="decision">
@@ -111,6 +137,7 @@ export default {
   },
   data: function() {
     return {
+      crate: 'crate',
       userSex: '',
       userAge: '',
       userWeight: '',
@@ -144,25 +171,47 @@ export default {
         ],
       temp_menu: [
         // place a copy of the menu in here
-        
+        {item: 'cesaersalad', cals: '350', info: [350, 30, 50, 10, 50], category: 'lunch/saladbar/gf/vegan', name: 'Cesaer Salad'},
+        {item: 'cheesecake', cals: '500', info: [500, 50, 100, 300, 100], category: 'dessert/justdesserts/vegetarian', name: 'Cheese Cake'},
+        {item: 'veggieburrito', cals: '400', info: [400, 20, 70, 0, 40], category: 'lunch/dinner/mexican/vegetarian/vegan', name: 'Veggie Burrito'},
+        {item: 'cheesepizza', cals: '300', info: [300, 300, 500, 10, 200], category: 'lunch/dinner/italian/vegetarian', name: 'Cheese Pizza'},
+        {item: 'bagelwithcreamcheese', cals: '250', info: [250, 30, 50, 10, 200], category: 'breakfast/jewish/vegetarian', name: 'Bagel w/ CC.'},
+        {item: 'chocolatecake', cals: '600', info: [600, 30, 50, 250, 100], category: 'dessert/justdesserts/vegetarian', name: 'Chocolate Cake'},
+        {item: 'homestylechickenbowl', cals: '690', info: [690, 30, 50, 20, 100], category: 'dinner/millcitygrill/gf', name: 'Chicken Bowl'},
+        {item: 'pastapennewithsauce', cals: '700', info: [700, 30, 50, 20, 100], category: 'dinner/italian', name: 'Penne w/ Sauce'},
+        {item: 'test', cals: '20', info: [20, 30, 50, 10, 10], category: 'breakfast/dessert/vegan/italian', name: 'Test Case'},
         ],
     }
   },
   watch: {
     raw_query() {
       this.update()
+    },
+    userAge() {
+      if (this.userAge <= 0) this.userAge = ''
+      if (this.userSex != '' && this.userWeight != '' && this.userAge != '') this.crate = 'crate g'
+      else this.crate = 'crate'
+    },
+    userWeight() {
+      if (this.userWeight <= 0) this.userWeight = ''
+      if (this.userSex != '' && this.userWeight != '' && this.userAge != '') this.crate = 'crate g'
+      else this.crate = 'crate'
+    },
+    userSex() {
+      if (this.userSex != '' && this.userWeight != '' && this.userAge != '') this.crate = 'crate g'
+      else this.crate = 'crate'
     }
-  },
+    },
   methods: {
-    sleep(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
+    sleep() {
+      return new Promise(resolve => setTimeout(resolve, 1));
     },
     // add n calories to total
     async increase(key, n) { 
     
       for (let i = 0; i < n; i++) {
 
-        await this.sleep(.1)
+        await this.sleep()
 
         switch (key) {
           case 'calories':
@@ -194,7 +243,7 @@ export default {
 
       for (let i = 0; i < n; i++) {
 
-          await this.sleep(.1)
+          await this.sleep()
 
           switch (key) {
             case 'calories':
@@ -431,8 +480,8 @@ export default {
 }
 .smalldecision {
   width: 100px;
-  margin-top: auto;
-  margin-bottom: auto;
+  margin-top: 1%;
+  margin-bottom: 1%;
 }
 .menuitems {
   margin-top: 3%;
@@ -479,34 +528,35 @@ export default {
   margin-top: 3%;
   margin-bottom: 3%;
   margin-left: 5%;
-  border-radius: 5px;
-  border-color: rgb(0, 0, 0);
-  height: 50px;
-  width: 150px;
-  font-size: xx-large;
+  height: 35px;
+  width: 134px;
+  font-size: large;
   font-weight: bold;
-  font-family: monospace;
+  font-family: consolas;
   cursor: pointer;
-  background-color: rgb(168, 191, 233);
-}
-.total:hover {
-  background-color: khaki;
+  background-color: rgb(255, 72, 40)
 }
 .box {
   background-color: rgb(214, 212, 221);
   width: 320px;
   padding-bottom: 400px;
   border: 3px solid black;
+  border-top: 0px;
   margin: 0px;
+}
+.wborder {
+  border-top: 3px solid black;
+  top: 0;
+  border-collapse: separate;
 }
 .xborder {
   border-left: none;
   border-right: none;
-  border-top: none;
+  border-top: 3px solid black;
   padding-bottom: 3%;
 }
 body {
-  margin: 3px;
+  margin: 0px;
 }
 .tab {
   margin-top: 1%;
@@ -521,9 +571,6 @@ header {
   top: 0; 
   width: 320px;
 }
-template {
-  margin: none;
-}
 #search::-webkit-search-cancel-button{
   position: relative;
   -webkit-appearance: none;
@@ -531,6 +578,28 @@ template {
   width: 10px;
   border-radius: 50%;
   background: rgb(251, 102, 102)
+}
+.crate {
+  margin: 4%;
+  padding: auto;
+  border: black solid 3px;
+  border-radius: 3px;
+  font-family: consolas;
+  font-weight: bold;
+}
+.r {
+  background-color: rgb(255, 126, 126);
+}
+.w {
+  background-color: white;
+}
+.g {
+  background-color: rgb(122, 212, 122);
+}
+.reduced {
+  margin-top: 1%;
+  margin-bottom: 1%;
+  margin-right: 1%;
 }
 
 </style>
